7fed1e313fd6d708d3abe205909c193e5725fe11
Mato0429 <91228634+Mato0429@users.noreply.github.com>
2026-08-08 10:27:46 +0900

    Update +page.svelte

--- rustnes-app/src/routes/+page.svelte
+++ rustnes-app/src/routes/+page.svelte
@@ -34,7 +34,7 @@
 
   async function frameLoop() {
     try {
-      let a = (+pressedKeys.has("KeyJ") || (+pressedKeys.has("KeyZ");
+      let a = +pressedKeys.has("KeyJ") || (+pressedKeys.has("KeyZ");
       let b = (+pressedKeys.has("KeyK") || (+pressedKeys.has("KeyX"))<< 1;
       let select = +pressedKeys.has("KeyF") << 2;
       let start = +pressedKeys.has("KeyH") << 3;
